1ee27af825fa17e03ba620a14074056376628f2f
sivakumar-mani <sivakumar.pmani@gmail.com>
2026-03-11 10:54:54 +0530

    updates
    updates

--- frontend/src/app/dialog/signup/signup.ts
+++ frontend/src/app/dialog/signup/signup.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { MatDialogActions, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { InputFormField } from '../../reusable/input-form-field/input-form-field';
+import { SelectFormField } from '../../reusable/select-form-field/select-form-field';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { globalConstants } from '../../services/global-constants';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-signup',
-  imports: [MatFormFieldModule, MatDialogModule, MatDialogActions, InputFormField, ReactiveFormsModule],
+  imports: [MatFormFieldModule,SelectFormField, CommonModule,  MatDialogModule, MatDialogActions, InputFormField, ReactiveFormsModule],
   templateUrl: './signup.html',
   styleUrl: './signup.scss',
 })
@@ -17,7 +19,10 @@ export class Signup {
   constructor(private dialog: MatDialogRef<Signup>,
     private fb: FormBuilder
   ) { }
-
+roleList = [
+  { value: 1, label: 'Admin' },
+  { value: 2, label: 'User' }
+];
   ngOnInit() {
     this.signupForm = this.fb.group({
       firstName: ['', Validators.required],
@@ -26,6 +31,8 @@ export class Signup {
       password: ['', [Validators.required, Validators.pattern(globalConstants.passwordRegex)]],
       email: ['', [Validators.required, Validators.pattern(globalConstants.emailRegex)]],
       contactNumber: ['', Validators.required],
+       role: ['', Validators.required],
+         status: ['', Validators.required],
     });
   }
 }
--- frontend/src/app/reusable/select-form-field/select-form-field.ts
+++ frontend/src/app/reusable/select-form-field/select-form-field.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
-
+import { CommonModule, NgIf, TitleCasePipe } from '@angular/common';
+import { Component, Input, Self } from '@angular/core';
+import { ControlValueAccessor, NgControl, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import { MatSelectModule} from '@angular/material/select';
+import { MatOption } from '@angular/material/core';
 @Component({
   selector: 'app-select-form-field',
-  imports: [],
+   imports: [ TitleCasePipe, NgIf, MatIconModule,CommonModule, MatOption, MatInputModule,MatSelectModule, ReactiveFormsModule, MatFormFieldModule],
   templateUrl: './select-form-field.html',
   styleUrl: './select-form-field.scss',
 })
-export class SelectFormField {
+export class SelectFormField implements ControlValueAccessor{
+  @Input() label : string='';
+  // @Input() options : { value : string | number; label: string}[]=[];
+  @Input() options: any[] = [];
+  @Input() labelClass: string ='';
+  @Input() selectClass: string='';
+
+  constructor(@Self() public ngControl : NgControl){
+    this.ngControl.valueAccessor = this;
+  }
+ get control(): FormControl{
+    return this.ngControl.control as FormControl;
+  }
+  writeValue(obj: any): void {}
+  registerOnChange(fn: any): void {}
+  registerOnTouched(fn: any): void {}
+  setDisabledState?(isDisabled: boolean): void {}
 
 }
